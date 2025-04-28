87741d835f48dab706fd8b13b0c3e536b707e433
v-xiaohuang <v-xiaohuang@microsoft.com>
2025-04-28 11:42:33 +0800

    add files

--- binding-object.js
+++ binding-object.js
@@ -23,3 +23,33 @@ function defineReactive(target, key, value) {
   })
 }
 Observe(person)
+
+var person ={name:'xiaoming', gender:'male', age: 25}
+function renderList(){
+    listHtml = ` I am ${person.name}. i am ${person.age} years old and i am ${person.gender}`
+    document.getElementsByTagName('body')[0].innerHTML = listHtml
+}
+
+function observe(obj){
+    if(typeof obj !== 'object' || obj === null) return 
+    for (var key in obj){
+        let value =  obj[key]
+        console.log('key', key)
+        console.log('obj', obj)
+        console.log('obj[key]', obj[key])
+        
+        Object.defineProperty(obj, key, {
+            get: function(){
+                return value
+            },
+            set: function(newVal){
+                if(value!==newVal){
+                    value=newVal
+                    renderList()
+                }
+            }
+        })
+    }
+    console.log('value', value)
+}
+observe(person)
